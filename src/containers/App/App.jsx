6bfdb64b8323bbe6e0adbde7189cdb92f3df19c4
Emily Dittmer <emilyedittmer@gmail.com>
2019-07-31 11:57:14 -0600

    Add additional styling to make page look slightly nicer

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,57 +1,58 @@
-import React, { Component } from 'react';
-import './App.scss';
-import { setAnimals, loadingComplete, hasErrored, setDonations } from '../../actions/index';
-import { connect } from 'react-redux';
-import AnimalsContainer from '../AnimalsContainer/AnimalsContainer';
-import Donations from '../Donations/Donations';
-import DonationForm from '../DonationForm/DonationForm'
+import React, { Component } from "react";
+import "./App.scss";
+import {
+  setAnimals,
+  loadingComplete,
+  hasErrored,
+  setDonations
+} from "../../actions/index";
+import { connect } from "react-redux";
+import AnimalsContainer from "../AnimalsContainer/AnimalsContainer";
+import Donations from "../Donations/Donations";
+import DonationForm from "../DonationForm/DonationForm";
 
 class App extends Component {
-  
-  componentDidMount(){
-    fetch('http://localhost:3001/api/v1/rescue-animals')
-    .then(response => {
-      if(!response.ok){
-        return error => this.props.hasErrored(error.message)
-      } else {
-        return response.json()
-      }
-    })
-    .then(animals => this.props.setAnimals(animals))
-    .then(() => this.props.loadingComplete())
-    .catch(error => this.props.hasErrored(error.message))
+  componentDidMount() {
+    fetch("http://localhost:3001/api/v1/rescue-animals")
+      .then(response => {
+        if (!response.ok) {
+          return error => this.props.hasErrored(error.message);
+        } else {
+          return response.json();
+        }
+      })
+      .then(animals => this.props.setAnimals(animals))
+      .then(() => this.props.loadingComplete())
+      .catch(error => this.props.hasErrored(error.message));
 
-    fetch('http://localhost:3001/api/v1/donations')
-    .then(response => {
-      if(!response.ok) {
-        return error => this.props.hasError(error.message)
-      } else {
-        return response.json()
-      }
-    })
-    .then(donations => this.props.setDonations(donations))
-    .then(() => this.props.loadingComplete())
-    .catch(error => this.props.hasErrored(error))
+    fetch("http://localhost:3001/api/v1/donations")
+      .then(response => {
+        if (!response.ok) {
+          return error => this.props.hasError(error.message);
+        } else {
+          return response.json();
+        }
+      })
+      .then(donations => this.props.setDonations(donations))
+      .then(() => this.props.loadingComplete())
+      .catch(error => this.props.hasErrored(error));
   }
-  
-  render() {
 
-    const loader = (
-      <h2>Page is Loading</h2>
-    )
-    const errorMessage = (
-      <h2>{this.props.error}</h2>
-    )
+  render() {
+    const loader = <h2>Page is Loading</h2>;
+    const errorMessage = <h2>{this.props.error}</h2>;
     return (
       <div className="App">
         <header>
           <h1>Animal Rescue</h1>
           {this.props.loading && loader}
-          {this.props.error !== '' && errorMessage}
+          {this.props.error !== "" && errorMessage}
         </header>
         <DonationForm />
-        <AnimalsContainer />
-        <Donations />
+        <main className='display-section'>
+          <AnimalsContainer />
+          <Donations />
+        </main>
       </div>
     );
   }
@@ -60,13 +61,16 @@ class App extends Component {
 const mapStateToProps = state => ({
   loading: state.loading,
   error: state.error
-})
+});
 
 const mapDispatchToProps = dispatch => ({
   setAnimals: animals => dispatch(setAnimals(animals)),
   loadingComplete: () => dispatch(loadingComplete()),
   hasErrored: message => dispatch(hasErrored(message)),
   setDonations: donations => dispatch(setDonations(donations))
-})
+});
 
-export default connect (mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/containers/DonationForm/DonationForm.jsx
+++ src/containers/DonationForm/DonationForm.jsx
@@ -1,72 +1,88 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import './DonationForm.scss';
-import { setAnimals, loadingComplete, hasErrored, setDonations, addDonation } from '../../actions/index';
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./DonationForm.scss";
+import {
+  setAnimals,
+  loadingComplete,
+  hasErrored,
+  setDonations,
+  addDonation
+} from "../../actions/index";
 
 class DonationForm extends Component {
   constructor() {
     super();
     this.state = {
-      name: '',
+      name: "",
       donationAmount: 0
-    }
+    };
   }
 
-  handleChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value })
-  }
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-  addNewDonation = (event) => {
-    event.preventDefault()
-    const user = {id:Date.now(), name:this.state.name, donation:this.state.donationAmount};
-    return fetch('http://localhost:3001/api/v1/donations/', {
-      method: 'POST',
+  addNewDonation = event => {
+    event.preventDefault();
+    this.resetInputs();
+    const user = {
+      id: Date.now(),
+      name: this.state.name,
+      donation: this.state.donationAmount
+    };
+    return fetch("http://localhost:3001/api/v1/donations/", {
+      method: "POST",
       body: JSON.stringify(user),
-      headers:{
-        'Content-Type': 'application/json'
+      headers: {
+        "Content-Type": "application/json"
       }
     })
-    .then(response => {
-      if(!response.ok) {
-        return error => this.props.hasErrored(error.message)
-      } else {
-        return response.json()
-      }
-    })
-    .then(donation => this.props.addDonation(donation))
-    .then(() => this.props.loadingComplete())
-    .catch(error => this.props.hasErrored(error.message))
-  }
+      .then(response => {
+        if (!response.ok) {
+          return error => this.props.hasErrored(error.message);
+        } else {
+          return response.json();
+        }
+      })
+      .then(donation => this.props.addDonation(donation))
+      .then(() => this.props.loadingComplete())
+      .catch(error => this.props.hasErrored(error.message));
+  };
+
+  resetInputs = () => {
+    this.setState({ name: "", donationAmount: 0 });
+  };
 
   render() {
-    return(
-      <form className='donation-form'>
-        <input 
-          type='text'
-          value={this.state.name}
-          placeholder='Name'
-          onChange = {event => this.handleChange(event)}
-          name='name'
-        />
-        <input 
-          type='number'
-          value={this.state.donationAmount}
-          placeholder='Donation Amount'
-          onChange = {event => this.handleChange(event)}
-          name='donationAmount'
-        />
-        <button
-          onClick={event => this.addNewDonation(event)}>Add Donation</button>
-      </form>
-    )
+    return (
+      <section className='donation-form-section'>
+        <h2 className='donation-form-header'>Add a Donation:</h2>
+        <form className="donation-form">
+          Your Name:<input
+            type="text"
+            value={this.state.name}
+            placeholder="Name"
+            onChange={event => this.handleChange(event)}
+            name="name"
+          />
+          Donation Amount:<input
+            type="number"
+            value={this.state.donationAmount}
+            placeholder="Donation Amount"
+            onChange={event => this.handleChange(event)}
+            name="donationAmount"
+          />
+          <button onClick={event => this.addNewDonation(event)}>Donate!</button>
+        </form>
+      </section>
+    );
   }
 }
 
 const mapStateToProps = state => ({
   loading: state.loading,
   error: state.error
-})
+});
 
 const mapDispatchToProps = dispatch => ({
   setAnimals: animals => dispatch(setAnimals(animals)),
@@ -74,6 +90,9 @@ const mapDispatchToProps = dispatch => ({
   hasErrored: message => dispatch(hasErrored(message)),
   setDonations: donations => dispatch(setDonations(donations)),
   addDonation: donation => dispatch(addDonation(donation))
-})
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(DonationForm);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DonationForm);
