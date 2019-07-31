44166f724b147eeecd82afd56d22998d5ec99019
Emily Dittmer <emilyedittmer@gmail.com>
2019-07-31 10:27:47 -0600

    Display AnimalCards without any styling

--- src/containers/AnimalCard/AnimalCard.jsx
+++ src/containers/AnimalCard/AnimalCard.jsx
@@ -2,8 +2,17 @@ import React, { Component } from 'react';
 
 class AnimalCard extends Component {
   render(){
+    const { name, species, description, img} = this.props
     return(
-      <h3>Hi</h3>
+      <article>
+        <h2>{name}</h2>
+        <img 
+          src={img}
+          alt={name}
+        />
+        <h3>{species}</h3>
+        <p>{description}</p>
+      </article>
     )
   }
 }
